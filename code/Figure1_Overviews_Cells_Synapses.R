11710d3f2a89e63b872a1a14df8009feeeaee516
Gaspar Jekely <g.jekely@exeter.ac.uk>
2021-07-07 16:15:39 +0100

    Conn: added http/1 connectivity to fix curl error for large calls

--- code/Figure1_Overviews_Cells_Synapses.R
+++ code/Figure1_Overviews_Cells_Synapses.R
@@ -4,7 +4,7 @@
 
 rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
 gc() #free up memrory and report the memory usage.
-Sys.setenv('R_MAX_VSIZE'=8000000000)
+Sys.setenv('R_MAX_VSIZE'=80000000000)
 
 # load nat and all associated packages, incl catmaid
 library(natverse)
@@ -17,69 +17,69 @@ require("graphics")
 
 # catmaid connection, needs username, password AND token - weird!
 # can run this separate file using source function
-source("~/R/conn.R")
+conn <- source("~/R/conn.R")
 workdir <- "/Users/gj274/OneDrive\ -\ University\ of\ Exeter/Paper/Connectome/Figures/Figure1_overview/"
 setwd(workdir)
 
+#see the available volumes
+catmaid_get_volumelist(conn = NULL, pid = 11)
+
+#read volumes
 outline <- catmaid_get_volume(1, rval = c("mesh3d", "catmaidmesh", "raw"),
   invertFaces = T, conn = NULL, pid = 11)
-
-catmaid_get_volumelist(conn = NULL, pid = 11)
 
 yolk <- catmaid_get_volume(4, rval = c("mesh3d", "catmaidmesh", "raw"),
   invertFaces = T, conn = NULL, pid = 11)
 
-neurons = nlapply(read.neurons.catmaid("connectome_neuron$", pid=11, fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-connectome = nlapply(read.neurons.catmaid("connectome$", pid=11, 
-                                          fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-chaeta = nlapply(read.neurons.catmaid("^chaeta$", pid=11, 
-                                          fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-acicula = nlapply(read.neurons.catmaid("^acicula$", pid=11, 
-                                      fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-muscle = nlapply(read.neurons.catmaid("^muscle$", pid=11, 
-                                       fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-endoderm = nlapply(read.neurons.catmaid("^endoderm$", pid=11, 
-                                      fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-epithelia = nlapply(read.neurons.catmaid("^epithelia_cell$", pid=11, 
-                                        fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-gland = nlapply(read.neurons.catmaid("^gland cell$", pid=11, 
-                                         fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-Ciliary_band_cell = nlapply(read.neurons.catmaid("^Ciliary_band_cell$", pid=11, 
-                                                 fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-glia = nlapply(read.neurons.catmaid("^glia cell$", pid=11, 
-                                                 fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
+#for larger calls we need to use http/1, see https://www.gitmemory.com/issue/natverse/rcatmaid/158/641537466
+#for this we configure to http/1.1
+conn_http1 = catmaid_login(conn=conn, config=httr::config(ssl_verifypeer=0, http_version=1.1))
+
+#catmaid_get_connector_table("^connectome$", pid= 11, direction = "incoming", conn = conn_http1)
+
+#read cells
+neurons = nlapply(read.neurons.catmaid("^connectome_neuron$", pid=11),
+                  function(x) smooth_neuron(x, sigma=6000), conn = conn_http1)
+
+connectome = nlapply(read.neurons.catmaid("^connectome$", pid=11, conn = conn_http1),
+                function(x) smooth_neuron(x, sigma=6000))
+Sensoryneuron = nlapply(read.neurons.catmaid("^connectome_Sensory_neuron$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+Motorneuron = nlapply(read.neurons.catmaid("^connectome_motorneuron$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+Interneuron = nlapply(read.neurons.catmaid("^connectome_interneuron$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+chaeta = nlapply(read.neurons.catmaid("^chaeta$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+acicula = nlapply(read.neurons.catmaid("^acicula$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+muscle = nlapply(read.neurons.catmaid("^muscle$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000), conn = conn_http1)
+endoderm = nlapply(read.neurons.catmaid("^endoderm$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+epithelia = nlapply(read.neurons.catmaid("^epithelia_cell$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000), conn = conn_http1)
+gland = nlapply(read.neurons.catmaid("^gland cell$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+Ciliary_band_cell = nlapply(read.neurons.catmaid("^Ciliary_band_cell$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
+glia = nlapply(read.neurons.catmaid("^glia cell$", pid=11),
+                function(x) smooth_neuron(x, sigma=6000))
 
 pnb = nlapply(read.neurons.catmaid("^pnb$", pid=11, 
                                     fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
 
-desmosome_connectome_new_non_muscle = nlapply(read.neurons.catmaid("^desmosome_connectome_new_non_muscle$", pid=11, 
-                                                                   fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-
 #these four dots are the most extreme points of the volume, adding them to the 3d view solves the problem with automatic zooming and movement of the field shown
 bounding_dots = nlapply(read.neurons.catmaid("^bounding_dots$", pid=11, 
                                                                    fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
-#nblast
-neurons_dots=dotprops(neurons/1e3, k=7, resample=1)
-blast_scores <- nblast_allbyall(neurons_dots)
-muscles_dots=dotprops(muscle/1e3, k=7, resample=1)
-blast_scores_mus <- nblast_allbyall(muscles_dots)
 
-# Hierarchically cluster the n.blast scores
-hckcs <- nhclust(scoremat=blast_scores)
-hckcs_mus <- nhclust(scoremat=blast_scores_mus)
-
-# Divide the hierarchical clustering into three groups
-library(dendroextras)
-dkcs <- colour_clusters(hckcs, k=4)
-
-# Plot a dendrogram of the clustering, with leaves labelled by true neuron type
-labels(dkcs) <- with(neurons_dots[labels(dkcs)], type)
-plot(dkcs)
-
-
+#check if there are any cells with two or more tagged somas
+sum = summary(gland)
+sum[sum$nsoma!=1,]
+as.numeric(rownames(sum[sum$nsoma==2,]))
 
 ##########################################
-# 3d plotting of clustering results
+# 3d plotting
 #########################################
 
 nopen3d() # opens apannable 3d window
@@ -95,14 +95,12 @@ plot3d(bounding_dots, WithConnectors = F, WithNodes = F, soma=F, lwd=1,
        col="white"
 ) 
 
-#plot3d(outline, WithConnectors = F, WithNodes = F, soma=F, lwd=2,
-#       rev = FALSE, fixup = F, add=T, forceClipregion = TRUE, alpha=0.04,
-#       col="#E2E2E2") 
 
 plot3d(yolk, WithConnectors = F, WithNodes = F, soma=F, lwd=2,
        rev = FALSE, fixup = F, add=T, forceClipregion = TRUE, alpha=0.1,
        col="#E2E2E2"
 ) 
+
 rgl.snapshot("connectome_body_1.png")
 
 plot3d(acicula, WithConnectors = F, WithNodes = F, soma=T, lwd=3,
